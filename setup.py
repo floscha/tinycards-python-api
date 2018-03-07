885527e1ed5dc93358960ee5792eaa576c38e7c1
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-03-07 09:46:14 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+NAME = 'tinycards'
+
+requirements = list(open('requirements.txt', 'r').readlines())
+
+setup(
+    name=NAME,
+    version='0.1',
+    description="An unofficial Python API for Tinycards by Duolingo",
+    url='https://github.com/floscha/tinycards-python-api',
+    author='Florian Schäfer',
+    author_email='florian.joh.schaefer@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=requirements,
+    zip_safe=False
+)
