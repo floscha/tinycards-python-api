79bf46768b3d73ea08b1cd991b1659046a983936
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-03-14 10:42:31 +0100

    Add pytest as test runner

---
+++ tests/client_tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from tinycards import Tinycards
+
+
+class ClientTest(unittest.TestCase):
+
+    def setUp(self):
+        self.client = Tinycards()
+
+    def test_get_trends_returns_10_decks(self):
+        trends = self.client.get_trends()
+
+        self.assertEqual(10, len(trends))
+
+
+if __name__ == '__main__':
+    unittest.main()
