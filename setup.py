2ae36ac32c710960bf23f3653bb6b38898466f8a
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-03-16 08:52:33 +0100

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -14,5 +14,15 @@ setup(
     license='MIT',
     packages=find_packages(),
     install_requires=requirements,
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development'
+    ]
 )
