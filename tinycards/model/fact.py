3f85935c925ae7e924a6058250ef071965949547
Reto Kaiser <reto@retokaiser.com>
2018-06-08 19:21:08 +0200

    Add fields "image_url" and "tts_url" for Fact

--- tinycards/model/fact.py
+++ tinycards/model/fact.py
@@ -5,11 +5,13 @@ from uuid import uuid4
 class Fact(object):
     """Data class for an Tinycards fact entity."""
 
-    def __init__(self, text, fact_id=None, fact_type=None):
+    def __init__(self, text=None, fact_id=None, fact_type=None, image_url=None, tts_url=None):
         """Initialize a new instance of the Fact class."""
         self.id = fact_id if fact_id else str(uuid4()).replace('-', '')
         self.text = text
         self.type = fact_type if fact_type else 'TEXT'
+        self.image_url = image_url
+        self.tts_url = tts_url
 
     def __str__(self):
         return str(self.__dict__)
