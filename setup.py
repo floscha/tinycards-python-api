d7e8c9af50e242faace242df88d25052e2c04a80
Reto Kaiser <reto@retokaiser.com>
2018-06-21 09:49:22 +0200

    Bump version to 0.22

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ NAME = 'tinycards'
 
 setup(
     name=NAME,
-    version='0.21',
+    version='0.22',
     description="An unofficial Python API for Tinycards by Duolingo",
     url='https://github.com/floscha/tinycards-python-api',
     author='Florian Schäfer',
