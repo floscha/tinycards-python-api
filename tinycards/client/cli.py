d9a46bf3d82f8ef621a6052a1995837cb7fc7e80
Florian Schäfer <florian.joh.schaefer@gmail.com>
2019-03-28 20:54:48 +0100

    Fix indent

--- tinycards/client/cli.py
+++ tinycards/client/cli.py
@@ -16,15 +16,15 @@ def main():
     args = sys.argv[1:]
 
     if len(args) == 2 and args[0] == 'decks' and args[1] == 'list':
-            client = Tinycards()
-            decks = client.get_decks()
-            if decks:
-                for d in decks:
-                    print(d.title)
-            else:
-                print("No decks found.")
+        client = Tinycards()
+        decks = client.get_decks()
+        if decks:
+            for d in decks:
+                print(d.title)
+        else:
+            print("No decks found.")
     elif len(args) == 3 and args[0] == 'decks' and args[1] == 'create':
-            client = Tinycards()
-            decks = client.create_deck(Deck(args[2]))
+        client = Tinycards()
+        decks = client.create_deck(Deck(args[2]))
     else:
         _print_usage_and_exit()
