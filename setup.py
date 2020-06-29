8e4c873f5e1a417610708d80e467564bdb5d6a21
Florian Schäfer <florian.joh.schaefer@gmail.com>
2020-06-29 22:04:45 +0200

    Update dependency versions

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        'requests==2.21.0',
+        'requests==2.24.0',
         'requests-toolbelt==0.9.1',
         'retrying==1.3.3',
-        'typer==0.0.8'
+        'typer==0.3.0'
     ],
     zip_safe=False,
     entry_points={
